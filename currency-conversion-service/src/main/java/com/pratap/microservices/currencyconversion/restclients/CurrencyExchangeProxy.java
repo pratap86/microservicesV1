36a968e3e0348debffbeb44e50b0b9db54a25ae3
pratap86 <narayanpratap86@gmail.com>
2021-02-04 22:16:56 +0530

    k8s changes along with their yml files

--- currency-conversion-service/src/main/java/com/pratap/microservices/currencyconversion/restclients/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/pratap/microservices/currencyconversion/restclients/CurrencyExchangeProxy.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import com.pratap.microservices.currencyconversion.entities.CurrencyConversion;
 
 //@FeignClient(name = "currency-exchange", url="localhost:8000")
-@FeignClient(name = "currency-exchange")
+//@FeignClient(name = "currency-exchange")
+//CHANGE-KUBERNETES
+@FeignClient(name = "currency-exchange", url = "${CURRENCY_EXCHANGE_SERVICE_HOST:http://localhost}:8000")
 public interface CurrencyExchangeProxy {
 
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
--- currency-exchange-service/src/main/java/com/pratap/microservices/currencyexchange/controllers/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/pratap/microservices/currencyexchange/controllers/CurrencyExchangeController.java
@@ -14,22 +14,27 @@ import com.pratap.microservices.currencyexchange.repositories.CurrencyExchangeRe
 @RestController
 public class CurrencyExchangeController {
 
-private static final Logger LOGGER = LoggerFactory.getLogger(CurrencyExchangeController.class);
-	
+	private static final Logger LOGGER = LoggerFactory.getLogger(CurrencyExchangeController.class);
+
 	@Autowired
 	private Environment environment;
-	
+
 	@Autowired
 	private CurrencyExchangeRepository currencyExchangeRepository;
 
 	@GetMapping("/from/{from}/to/{to}")
 	public CurrencyExchange retrieveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to) {
-		
+
 		LOGGER.info("retrieveExchangeValue called with {} and {}", from, to);
 		CurrencyExchange currencyExchange = currencyExchangeRepository.findByFromAndTo(from, to);
-		if(currencyExchange == null) throw new RuntimeException("Unable to find data from "+from+" to "+to);
-		currencyExchange.setEnvironment(environment.getProperty("local.server.port"));
+		if (currencyExchange == null)
+			throw new RuntimeException("Unable to find data from " + from + " to " + to);
+		String port = environment.getProperty("local.server.port");
+		// CHANGE-KUBERNETES
+		String host = environment.getProperty("HOSTNAME");
+		String version = "v11";
+		currencyExchange.setEnvironment(port + " " + version + " " + host);
 		return currencyExchange;
 	}
-	
+
 }
